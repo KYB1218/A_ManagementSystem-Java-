9917248d80d343a517156390e64b4882531f7b62
ha819914 <ha819914@gmail.com>
2023-11-27 14:10:41 +0900

    git commit

---
+++ app/src/main/java/com/example/ems/Equipment_item.java
@@ -0,0 +1,43 @@
+package com.example.ems;
+
+public class Equipment_item {
+    private String  id;
+    private String name;
+    private String detail;
+    private String state;
+
+    public Equipment_item() {
+    }
+
+    public String  getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
---
+++ app/src/main/java/com/example/ems/RecyclerViewInterface.java
@@ -0,0 +1,5 @@
+package com.example.ems;
+
+public interface RecyclerViewInterface {
+    void onItemClick(int position);
+}
